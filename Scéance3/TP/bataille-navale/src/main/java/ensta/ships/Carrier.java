1eebb742b20bc041f7fcd98f517324af11833f19
alexandre <alexandrebodinier@icloud.com>
2020-02-04 12:04:03 +0100

    exo2

---
+++ Scéance3/TP/bataille-navale/src/main/java/ensta/ships/Carrier.java
@@ -0,0 +1,18 @@
+package ensta.ships;
+
+public class Carrier extends AbstractShip {
+
+    public Carrier(String cname, char clabel, Direction cdir){
+        super(cname, clabel, 5, cdir);
+    }
+
+    /**
+     * @return default constructor that initializes size at 2 and dir at EAST
+     */
+    public Carrier(){
+        super("",'c',5,Direction.EAST);
+    }
+    public String toString(){
+        return this.name + " " + this.label + " " + this.size + " " + this.dir;
+    }
+}
