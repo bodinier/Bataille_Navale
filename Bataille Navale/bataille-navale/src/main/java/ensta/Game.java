af3bd01b75858dcba7aeb05855fbe0a713b6a379
Alexandre Bodinier <48906594+bodinier@users.noreply.github.com>
2020-02-26 00:49:03 +0100

    Final Version

--- Bataille Navale/bataille-navale/src/main/java/ensta/Game.java
+++ Bataille Navale/bataille-navale/src/main/java/ensta/Game.java
@@ -75,7 +75,6 @@ public class Game {
 
             done = updateScore();
             b1.print();
-            b2.print();
             
             System.out.println(makeHitMessage(false /* outgoing hit */, coords, hit));
 
